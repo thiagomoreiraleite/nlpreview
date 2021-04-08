d97046574e5ae306e1afad8148283526e6134d1c
Pedro Franca <pedro.franca@barcelonagse.eu>
2021-04-08 19:48:51 +0200

    refactoring app.py

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import Sentimedia.data_viz as dv
 import Sentimedia.trainer as tr
 
-# import spacy_streamlit
-# from streamlit_lottie import st_lottie
+
 from pyecharts import options as opts
 from pyecharts.charts import Bar
 from pyecharts.charts import WordCloud
 from streamlit_echarts import st_pyecharts
 from streamlit_echarts import st_echarts
 
-# import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 import streamlit.components.v1 as components
 import base64
 import requests
 import time
 from PIL import Image
+
+import pandas as pd
 
 favicon = Image.open('favicon.png')
 
@@ -26,10 +26,9 @@ st.set_page_config(
             page_icon=favicon,
             layout="wide")
 
-import numpy as np
-import pandas as pd
 
-#LAYING OUT THE SIDE BAR
+
+########### LAYING OUT THE SIDE BAR ###########
 st.sidebar.markdown(f"""
   # CONTROL PANEL
 """)
@@ -45,8 +44,7 @@ st.sidebar.markdown(f"""
 st.sidebar.header('Word Cloud and Bar Chart Selections')
 double_entry = st.sidebar.radio('Benchmark your business to others', ('Single View', 'Display Benchmark'))
 
-rest_name_input2 = 'Parish Cafe and Bar'
-rest_name_input3 = 'Tatte Bakery & Cafe'
+
 rest_name_input2 = st.sidebar.text_input('Business Name', 'Parish Cafe and Bar', key='rest_name_input2')
 if double_entry == 'Display Benchmark':
   st.sidebar.subheader('Select a business for Benchmarking')
@@ -57,8 +55,8 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
-# LAYING OUT THE TOP SECTION OF THE APP
 
+########### LAYING OUT THE TOP SECTION OF THE APP ###########
 ELEMENT_HTML = f"""
 <div id="logo">
 </div>
@@ -66,7 +64,7 @@ ELEMENT_HTML = f"""
 st.write(ELEMENT_HTML, unsafe_allow_html=True)
 
 st.warning('Designed for demo purpose, this app uses only a very reduced sample of businesses')
-st.info('Source code: https://github.com/pedro-franca/nlpreview and https://github.com/thiagomoreiraleite/nlpreview')
+st.info('Source code: https://github.com/pedro-franca/nlpreview')
 
 row1_1, row1_2, row1_3 = st.beta_columns((8,2,13))
 with row1_1:
@@ -75,8 +73,8 @@ with row1_1:
     </div>
   """
   st.write(WELCOME_HTML, unsafe_allow_html=True)
-  st.markdown(""" 
-    ## A visual aid tool for sentiment analysis with your business reviews. 
+  st.markdown("""
+    ## A visual aid tool for sentiment analysis with your business reviews.
     ### Please, feel free to try different approaches in the control panel on the left, as well as our predictive model below.
     ### Locate businesses in the map by city and rating; find your business and other to benchmark.
     ### Enjoy the beautiful insights!
@@ -93,8 +91,8 @@ with row1_3:
 
 st.markdown("""# """)
 
-#STOP WORDS SELECTIONS
-  
+########### STOP WORDS SELECTIONS ###########
+
 def make_checkbox_one(words):
   words_list = [x[0] for x in words[:10]]
   checkbox = []
@@ -126,19 +124,21 @@ def cached_checkbox(data_list, check_one, check_two, check_three):
 def button_states():
     return {"pressed": None}
 
-#VISUALIZATIONS
+########### WORDCLOUD + BARPLOT ###########
 
 c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, [], [])
-c_positive_bench, c_negative_bench, data_positive_bench, data_negative_bench = dv.make_wordcloud_interactive(rest_name_input3, [], [])
 b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, [], [])
-b_pos_bench, b_neg_bench = dv.make_barplot_interactive(rest_name_input3, [], [])
+
+if double_entry == 'Display Benchmark':
+  c_positive_bench, c_negative_bench, data_positive_bench, data_negative_bench = dv.make_wordcloud_interactive(rest_name_input3, [], [])
+  b_pos_bench, b_neg_bench = dv.make_barplot_interactive(rest_name_input3, [], [])
 
 HEADER_upload = f"""
   <div><h2>FIND OUT THE SENTIMENT OF UPLOADED REVIEWS</h2>
   </div>
 """
 
-#DISPLAYING CSV UPLOADED DATA
+########### DISPLAYING CSV UPLOADED DATA ###########
 
 st.write(HEADER_upload, unsafe_allow_html=True)
 st.subheader("Upload reviews in CSV format on the Control Panel of the Sidebar to visualize if they are positive or negative")
@@ -155,21 +155,26 @@ if uploaded_file is not None:
     count = pd.DataFrame(count)
     count.columns = ['Count']
     st.write(count)
-    
+
 
 else:
   st.markdown("""## """)
   st.markdown("""## """)
 
-if double_entry == 'Display Benchmark':
+if double_entry == 'Single View':
   HEADER_HTML = f"""
-  <div><h1>GET INSIGHTFUL VISUAL INFORMATION</h1>
-  </div>
+    <div><h1>GET INSIGHTFUL VISUAL INFORMATION</h1>
+    </div>
   """
   st.write(HEADER_HTML, unsafe_allow_html=True)
   st.subheader("Improve the visualizations")
+
+  ########### SINGLE VIEW POSITIVE REVIEWS VISUALIZATIONS AND STOPWORDS #########
+
   st.text('select stop words below for POSITIVE reviews')
-#DOUBLE VIEW POSITIVE REVIEWS VISUALIZATIONS AND STOPWORDS
+
+  checked_pos_words = []
+  checked_neg_words = []
   col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
   with col1:
     st.markdown("""# """)
@@ -181,12 +186,105 @@ if double_entry == 'Display Benchmark':
     st.markdown("""# """)
     checkboxes_pos_three = make_checkbox_three(data_positive)
     st.markdown("""# """)
-    # words_list_pos = [x[0] for x in data_positive]
-    # checked_pos_words = [word for word, checked in zip(words_list_pos, checkboxes_pos_one + checkboxes_pos_two + checkboxes_pos_three) if checked]
     checked_pos_words = cached_checkbox(data_positive, checkboxes_pos_one, checkboxes_pos_two, checkboxes_pos_three)
-    # if st.button("Remove stop words", key="positive"):
-    #   c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, [])
-    #   b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, [])
+
+    press_button = st.button("Remove stop words", key="positive")
+    is_pressed = button_states()
+    if press_button:
+      # any changes need to be performed in place
+      is_pressed.update({"pressed": True})
+
+    if is_pressed["pressed"]:
+      c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, [])
+      b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, [])
+  with col4:
+    st.markdown("""## """)
+  with col5:
+    st_pyecharts(c_positive,
+      theme={
+        "width": "1000",
+        "height": "800",
+        "subtitle_text_size": "20",
+      },
+    )
+    st_pyecharts(
+      b_pos,
+      theme={
+          "backgroundColor": "#afc3a1c7",
+          "textStyle": {"color": "#F63366"},
+          "yaxis_name_pos" : "end",
+      },
+    )
+  st.markdown("""## """)
+
+########### SINGLE VIEW NEGATIVE REVIEWS VISUALIZATIONS AND STOPWORDS #########
+  st.text('select stop words below for NEGATIVE reviews')
+
+  col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
+  with col1:
+    st.markdown("""# """)
+    checkboxes_neg_one = make_checkbox_one(data_negative)
+  with col2:
+    st.markdown("""# """)
+    checkboxes_neg_two = make_checkbox_two(data_negative)
+  with col3:
+    st.markdown("""# """)
+    checkboxes_neg_three = make_checkbox_three(data_negative)
+    st.markdown("""# """)
+    checked_neg_words = cached_checkbox(data_negative, checkboxes_neg_one, checkboxes_neg_two, checkboxes_neg_three)
+
+    press_button = st.button("Remove stop words", key="negative")
+    is_pressed = button_states()
+    if press_button:
+      # any changes need to be performed in place
+      is_pressed.update({"pressed": True})
+
+    if is_pressed["pressed"]:
+      c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
+      b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
+  with col4:
+    st.markdown("""## """)
+  with col5:
+    st_pyecharts(c_negative,
+      theme={
+        "width": "1000",
+        "height": "800",
+        "subtitle_text_size": "20px",
+      },
+    )
+    st_pyecharts(
+      b_neg,
+      theme={
+          "backgroundColor": '#f4cccc',
+          "textStyle": {"color": "#F63366"},
+          "yaxis_name_pos" : "end",
+      },
+    )
+
+########### DISPLAY BENCHMARK VISUALIZATIONS AND STOPWORDS ###
+else:
+  HEADER_HTML = f"""
+  <div><h1>GET INSIGHTFUL VISUAL INFORMATION</h1>
+  </div>
+  """
+  st.write(HEADER_HTML, unsafe_allow_html=True)
+  st.subheader("Improve the visualizations")
+  st.text('select stop words below for POSITIVE reviews')
+
+########### DISPLAY BENCHMARK POSITIVE REVIEWS VISUALIZATIONS AND STOPWORDS ###
+  col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
+  with col1:
+    st.markdown("""# """)
+    checkboxes_pos_one = make_checkbox_one(data_positive)
+  with col2:
+    st.markdown("""# """)
+    checkboxes_pos_two = make_checkbox_two(data_positive)
+  with col3:
+    st.markdown("""# """)
+    checkboxes_pos_three = make_checkbox_three(data_positive)
+    st.markdown("""# """)
+
+    checked_pos_words = cached_checkbox(data_positive, checkboxes_pos_one, checkboxes_pos_two, checkboxes_pos_three)
     press_button = st.button("Remove stop words", key="positive")
     is_pressed = button_states()
     if press_button:
@@ -197,7 +295,7 @@ if double_entry == 'Display Benchmark':
       c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, [])
       b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, [])
   with col4:
-    st.markdown("""## """) 
+    st.markdown("""## """)
   with col5:
     st_pyecharts(c_positive,
       theme={
@@ -227,11 +325,7 @@ if double_entry == 'Display Benchmark':
     st.markdown("""# """)
     checkboxes_pos_three_bench = make_checkbox_three(data_positive_bench)
     st.markdown("""# """)
-    # words_list_pos_bench = [x[0] for x in data_positive_bench]
-    # checked_pos_words_bench = [word for word, checked in zip(words_list_pos_bench, checkboxes_pos_one_bench + checkboxes_pos_two_bench + checkboxes_pos_three_bench) if checked]
-    # if st.button("Remove stop words", key="positive_bench"):
-    #   c_positive_bench, c_negative_bench, data_positive_bench, data_negative_bench = dv.make_wordcloud_interactive(rest_name_input3, checked_pos_words_bench, [])
-    #   b_pos_bench, b_neg_bench = dv.make_barplot_interactive(rest_name_input3, checked_pos_words, [])
+
     checked_pos_words_bench = cached_checkbox(data_positive_bench, checkboxes_pos_one_bench, checkboxes_pos_two_bench, checkboxes_pos_three_bench)
     press_button = st.button("Remove stop words", key="positive_bench")
     is_pressed = button_states()
@@ -261,7 +355,8 @@ if double_entry == 'Display Benchmark':
       },
     )
   st.text('select stop words below for NEGATIVE reviews')
-#DOUBLE VIEW NEGATIIVE REVIEWS VISUALIZATIONS AND STOPWORDS
+
+########### DISPLAY BENCHMARK NEGATIIVE REVIEWS VISUALIZATIONS AND STOPWORDS ##
   col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
   with col1:
     st.markdown("""# """)
@@ -273,12 +368,8 @@ if double_entry == 'Display Benchmark':
     st.markdown("""# """)
     checkboxes_neg_three = make_checkbox_three(data_negative)
     st.markdown("""# """)
-    # words_list_neg = [x[0] for x in data_negative]
-    # checked_neg_words = [word for word, checked in zip(words_list_neg, checkboxes_neg_one + checkboxes_neg_two + checkboxes_neg_three) if checked]
-    # if st.button("Remove stop words", key="negative"):
-    #   c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, [], checked_neg_words)
-    #   b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, [], checked_neg_words)
     checked_neg_words = cached_checkbox(data_negative, checkboxes_neg_one, checkboxes_neg_two, checkboxes_neg_three)
+
     press_button = st.button("Remove stop words", key="negative")
     is_pressed = button_states()
     if press_button:
@@ -289,7 +380,7 @@ if double_entry == 'Display Benchmark':
       c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, [], checked_neg_words)
       b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, [], checked_neg_words)
   with col4:
-    st.markdown("""## """) 
+    st.markdown("""## """)
   with col5:
     st_pyecharts(c_negative,
       theme={
@@ -319,12 +410,8 @@ if double_entry == 'Display Benchmark':
     st.markdown("""# """)
     checkboxes_neg_three_bench = make_checkbox_three(data_negative_bench)
     st.markdown("""# """)
-    # words_list_neg_bench = [x[0] for x in data_negative_bench]
-    # checked_neg_words_bench = [word for word, checked in zip(words_list_neg_bench, checkboxes_neg_one_bench + checkboxes_neg_two_bench + checkboxes_neg_three_bench) if checked]
-    # if st.button("Remove stop words", key="negative_bench"):
-    #   c_positive_bench, c_negative_bench, data_positive_bench, data_negative_bench = dv.make_wordcloud_interactive(rest_name_input3, [], checked_neg_words_bench)
-    #   b_pos_bench, b_neg_bench = dv.make_barplot_interactive(rest_name_input3, [], checked_neg_words)
     checked_neg_words_bench = cached_checkbox(data_negative_bench, checkboxes_neg_one_bench, checkboxes_neg_two_bench, checkboxes_neg_three_bench)
+
     press_button = st.button("Remove stop words", key="negative_bench")
     is_pressed = button_states()
     if press_button:
@@ -353,113 +440,7 @@ if double_entry == 'Display Benchmark':
       },
     )
 
-else:
-  HEADER_HTML = f"""
-    <div><h1>GET INSIGHTFUL VISUAL INFORMATION</h1>
-    </div>
-  """
-  st.write(HEADER_HTML, unsafe_allow_html=True)
-  st.subheader("Improve the visualizations")
-  st.text('select stop words below for POSITIVE reviews')
-#SINGLE VIEW POSITIVE REVIEWS VISUALIZATIONS AND STOPWORDS
-  checked_pos_words = []
-  checked_neg_words = []
-  col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
-  with col1:
-    st.markdown("""# """)
-    checkboxes_pos_one = make_checkbox_one(data_positive)
-  with col2:
-    st.markdown("""# """)
-    checkboxes_pos_two = make_checkbox_two(data_positive)
-  with col3:
-    st.markdown("""# """)
-    checkboxes_pos_three = make_checkbox_three(data_positive)
-    st.markdown("""# """)
-    # words_list_pos = [x[0] for x in data_positive]
-    # checked_pos_words = [word for word, checked in zip(words_list_pos, checkboxes_pos_one + checkboxes_pos_two + checkboxes_pos_three) if checked]
-    # if st.button("Remove stop words", key="positive"):
-    #   c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, [])
-    #   b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
-    checked_pos_words = cached_checkbox(data_positive, checkboxes_pos_one, checkboxes_pos_two, checkboxes_pos_three)
-    press_button = st.button("Remove stop words", key="positive")
-    is_pressed = button_states()
-    if press_button:
-      # any changes need to be performed in place
-      is_pressed.update({"pressed": True})
 
-    if is_pressed["pressed"]:
-      c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, [])
-      b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, [])
-  with col4:
-    st.markdown("""## """)
-  with col5:
-    st_pyecharts(c_positive,
-      theme={
-        "width": "1000",
-        "height": "800",
-        "subtitle_text_size": "20",
-      },
-    )
-    st_pyecharts(
-      b_pos,
-      theme={
-          "backgroundColor": "#afc3a1c7",
-          "textStyle": {"color": "#F63366"},
-          "yaxis_name_pos" : "end",
-      },
-    )
-  st.markdown("""## """)
- 
-#SINGLE VIEW NEGATIVE REVIEWS VISUALIZATIONS AND STOPWORDS
-  st.text('select stop words below for NEGATIVE reviews')
-  col1, col2, col3, col4, col5 = st.beta_columns((2,2,2,1,14))
-  with col1:
-    st.markdown("""# """)
-    checkboxes_neg_one = make_checkbox_one(data_negative)
-  with col2:
-    st.markdown("""# """)
-    checkboxes_neg_two = make_checkbox_two(data_negative)
-  with col3:
-    st.markdown("""# """)
-    checkboxes_neg_three = make_checkbox_three(data_negative)
-    st.markdown("""# """)
-    # words_list_neg = [x[0] for x in data_negative]
-    # checked_neg_words = [word for word, checked in zip(words_list_neg, checkboxes_neg_one + checkboxes_neg_two + checkboxes_neg_three) if checked]
-    # if st.button("Remove stop words", key="negative"):
-    #   c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
-    #   b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
-    #   st.write(checked_pos_words)
-    checked_neg_words = cached_checkbox(data_negative, checkboxes_neg_one, checkboxes_neg_two, checkboxes_neg_three)
-    press_button = st.button("Remove stop words", key="negative")
-    is_pressed = button_states()
-    if press_button:
-      # any changes need to be performed in place
-      is_pressed.update({"pressed": True})
-
-    if is_pressed["pressed"]:
-      c_positive, c_negative, data_positive, data_negative = dv.make_wordcloud_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
-      b_pos, b_neg = dv.make_barplot_interactive(rest_name_input2, checked_pos_words, checked_neg_words)
-  with col4:
-    st.markdown("""## """)
-  with col5:
-    st_pyecharts(c_negative,
-      theme={
-        "width": "1000",
-        "height": "800",
-        "subtitle_text_size": "20px",
-      },
-    )
-    st_pyecharts(
-      b_neg,
-      theme={
-          # "backgroundColor": "#a4bbd5bf",
-          "backgroundColor": '#f4cccc',
-          "textStyle": {"color": "#F63366"},
-          "yaxis_name_pos" : "end",
-      },
-    )
-
-    
 st.markdown("""# """)
 HEADER_HTML_BOTTOM = f"""
   <div><h1>WORD FREQUENCY ACROSS CATEGORIES</h1>
@@ -470,11 +451,11 @@ st.subheader("Find mentions for each word by selecting on the chart or through t
 html = dv.get_sct_html(rest_name_input2, city_name_input)
 
 HtmlFile = open("rest_reviews-Vis.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
+source_code = HtmlFile.read()
 components.html(source_code, height = 50000)
 
 
-#STYLING CODE
+########### STYLING CODE ###########
 
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
@@ -505,7 +486,7 @@ def set_png_as_page_bg2(png_file):
     }
     </style>
     ''' % bin_str
-    
+
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 set_png_as_page_bg2('logo2.png')
@@ -528,5 +509,5 @@ p {
   margin-top: 0;
   padding-top: 0;
 }
-"""  
+"""
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
